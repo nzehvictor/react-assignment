a929c0f0f37ae602e80f084069d581380db51a86
nzehvictor <nzehchukwualuka@gmail.com>
2022-07-09 08:16:58 +0100

    a

--- src/ExpenseTracker.js
+++ src/ExpenseTracker.js
@@ -3,21 +3,39 @@ import React from 'react'
 export default function ExpenseTracker() {
   return (
     <>
-      <main style={{textAlign:"center"}}>
+      <main style={{ textAlign: "center" }}>
         <h1>Expense Tracker</h1>
         <div>
           <p>Your Balance</p>
           <h1 id="balance">$500,000</h1>
         </div>
         <div id="divOne">
-          <table id="tableOne">
+          <table
+            style={{
+              width: "70%",
+              marginLeft: "15%",
+              marginRight: "15%",
+              textAlign: "center",
+              padding: "10px 40px",
+              borderRadius: "10px",
+              boxShadow: "0px 0px 3px 3px",
+            }}>
             <tr>
               <th>INCOME</th>
               <th>EXPENSE</th>
             </tr>
             <tr>
-              <td id="income">$5000</td>
-              <td id="expense">$4000</td>
+              <td
+                style={{
+                  color: "green",
+                  fontSize: "35px",
+                  borderRight: "1px solid black",
+                }} >$5000</td>
+              <td
+                style={{
+                  color: "red",
+                  fontSize: "35px"
+                }}>$4000</td>
             </tr>
           </table>
         </div>
@@ -29,10 +47,10 @@ export default function ExpenseTracker() {
         <div>
           <h3>Add New Transactions</h3>
           <label for="">Text</label><br />
-          <input type="text" id="inputStr" style={{width:"30%"}}/><br />
+          <input type="text" id="inputStr" style={{ width: "30%" }} /><br />
           <label for="">Amount  (negative-expense positive-income)</label><br />
-          <input type="text" id="inputNum" style={{width:"30%"}}/><br />
-          <button style={{width:"20%"}}>Add Transaction</button>
+          <input type="text" id="inputNum" style={{ width: "30%" }} /><br />
+          <button style={{ width: "20%" }}>Add Transaction</button>
         </div>
       </main>
     </>
